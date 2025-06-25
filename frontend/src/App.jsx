4ca74feaf6a43fe39cb3e9b124db5a8a3bbcdc6a
himanshux88 <himanshujindal201@gmail.com>
2025-06-25 13:46:49 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import CaptainHome from "./pages/CaptainHome";
 import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
 import Riding from "./pages/Riding";
 import CaptainRiding from "./pages/CaptainRiding";
+import { LoadScript } from "@react-google-maps/api";
 
 const App = () => {
   return (
